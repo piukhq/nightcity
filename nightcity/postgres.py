fca19b7dc4403a3205573447c663197882e1e80e
Chris Pressland <mail@cpressland.io>
2024-02-04 13:42:01 +0000

    Improve token generation

--- nightcity/postgres.py
+++ nightcity/postgres.py
@@ -5,12 +5,14 @@ from sqlalchemy.engine.base import Engine
 
 from nightcity.settings import identity, settings
 
+token: str = identity.get_token("https://ossrdbms-aad.database.windows.net/.default").token
+
 
 def pg_connection(database_name: str = "postgres") -> connection:
     """Establish a connection to Postgres using Token Authentication."""
     return psycopg2.connect(
         user=settings.postgres_user,
-        password=identity.get_token("https://ossrdbms-aad.database.windows.net/.default").token,
+        password=token,
         host=settings.postgres_host,
         port=5432,
         dbname=database_name,
@@ -21,5 +23,5 @@ def pg_connection(database_name: str = "postgres") -> connection:
 def sqla_connection(database_name: str = "postgres") -> Engine:
     """Establish a connection to Postgres using Token Authentication."""
     return create_engine(
-        f"postgresql+psycopg2://{settings.postgres_user}:{identity.get_token('https://ossrdbms-aad.database.windows.net/.default').token}@{settings.postgres_host}:5432/{database_name}?sslmode=require",
+        f"postgresql+psycopg2://{settings.postgres_user}:{token}@{settings.postgres_host}:5432/{database_name}?sslmode=require",
     )
