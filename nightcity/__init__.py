8d540eb2d1ac0d9ddbf8fb6c9d195db0e808c3a1
Thenuja Viknarajah <thenuja_1@outlook.com>
2024-03-14 14:21:14 +0000

    imports within function

--- nightcity/__init__.py
+++ nightcity/__init__.py
@@ -2,12 +2,7 @@
 
 import typer
 
-from nightcity.brendan import run as brendan
-from nightcity.delamain import run as delamain
-from nightcity.screamsheet import send_marketing_info, send_transcation_info
-
 cli = typer.Typer()
-
 screamsheet = typer.Typer()
 cli.add_typer(screamsheet, name="screamsheet")
 
@@ -15,24 +10,32 @@ cli.add_typer(screamsheet, name="screamsheet")
 @cli.command(name="brendan")
 def cli_brendan() -> None:
     """Add/Remove users from Postgres based on Entra ID Group Membership."""
+    from nightcity.brendan import run as brendan
+
     brendan()
 
 
 @cli.command(name="delamain")
 def cli_delamain() -> None:
     """Configure NextDNS Rewrites based on DNS Lookups from within Kubernetes."""
+    from nightcity.delamain import run as delamain
+
     delamain()
 
 
 @screamsheet.command(name="marketing")
 def marketing() -> None:
     """Send Marketing Preference to Viator."""
+    from nightcity.screamsheet import send_marketing_info
+
     send_marketing_info()
 
 
 @screamsheet.command(name="transactions")
 def transactions() -> None:
     """Send Transactions to Viator."""
+    from nightcity.screamsheet import send_transcation_info
+
     send_transcation_info()
 
 
