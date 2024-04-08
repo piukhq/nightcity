3a63356d5e3c987a83ddefc437c7102d87bb1bd4
Chris Pressland <mail@cpressland.io>
2024-04-08 20:45:39 +0100

    Add missing CLI

--- nightcity/__init__.py
+++ nightcity/__init__.py
@@ -4,7 +4,7 @@ from sys import platform
 
 import typer
 
-from nightcity import brendan, ncart, ozob, screamsheet
+from nightcity import brendan, ncart, ozob, quickhacks, screamsheet
 
 if platform == "linux":
     typer.core.rich = None
@@ -15,6 +15,7 @@ cli.add_typer(brendan.app, name="brendan", help="PIM Functions.")
 cli.add_typer(ozob.app, name="ozob", help="SFTP Functions.")
 cli.add_typer(ncart.app, name="ncart", help="Blob Storage Functions.")
 cli.add_typer(screamsheet.app, name="screamsheet", help="Viator Functions.")
+cli.add_typer(quickhacks.app, name="quickhacks", help="Hacky Functions.")
 
 
 if __name__ == "__main__":
